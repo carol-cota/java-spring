40e783872b1a85f098bfdd17cd5232b155ef669d
Diogo Lundberg <dclundberg@gmail.com>
2022-03-07 13:06:14 +0100

    Add jetty

--- src/main/java/myPackage/App.java
+++ src/main/java/myPackage/App.java
@@ -1,32 +1,24 @@
 package myPackage;
 
-import myPackage.config.AppConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.WebApplicationInitializer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.Dynamic;
-import javax.swing.*;
 
-public class App implements WebApplicationInitializer {
+public class App {
+    public static void main(String[] params) throws Exception {
+        //SPRING
+        AnnotationConfigWebApplicationContext webApplication = new AnnotationConfigWebApplicationContext();
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplication);
 
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.register(AppConfig.class);
-        applicationContext.setServletContext(servletContext);
-
-        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(applicationContext));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
-    }
-
-    public static void main(String[] params) {
-        ApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.
+        // JETTY
+        ServletContextHandler contextHandler = new ServletContextHandler();
+        contextHandler.addServlet(new ServletHolder(dispatcherServlet), "/");
+        Server server = new Server(8080);
+        server.setHandler(contextHandler);
+        server.start();
+        server.join();
     }
 }
--- src/main/java/myPackage/config/AppConfig.java
+++ src/main/java/myPackage/config/AppConfig.java
@@ -1,6 +1,5 @@
 package myPackage.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,14 +8,4 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan("myPackage")
 @EnableWebMvc
 public class AppConfig {
-
-//    @Bean("courseRepository")
-//    public CourseRepository getCourseRepository() {
-//        return new CourseRepository();
-//    }
-//
-//    @Bean("courseService")
-//    public CourseService getCourseService() {
-//        return new CourseService(getCourseRepository());
-//    }
 }
